5b202fa8f424aaeb7122e467f6c5c49456cc751e
Natalya Rapstine <natalya.patrikeeva@gmail.com>
2017-05-05 08:59:44 -0600

    add question 4 solution

--- 4_merging_ranges.py
+++ 4_merging_ranges.py
@@ -3,32 +3,76 @@
 # (start_time, end_time). These integers represent the number of 30-minute
 # blocks past 9:00 am.
 
+# Brute force. Compare every meeting to every other meeting. O(n^2) time.
+
+# Efficient approach.
+
 def merge_ranges(input_list):
 
-    output = []
-    start = input_list[0][0]
-    end = input_list[0][1]
-    output.append( [start, end ])
+    # Sort first by start time
+    # Soring in place O(n log n) time
+    input_list.sort(key=lambda tup: tup[0])
 
-    for index, tup in enumerate( input_list[1:] ):
-        print tup
-        if tup[0] > start and tup[0] < end :
-            output[1][1] = tup[1]
+    output_list = []
 
-        elif tup[1] >= start and tup[1] <= end :
-            output[2][0] = tup[0]
+    # Merge
+    # Go through the list once merging adjacent tuples
+
+    tmp = [input_list[0][0], input_list[0][1]]
+
+    for i, tup in enumerate(input_list[:-1]) :
+
+        # if start of second tuple is between start and end of the first tuple
+        if input_list[i + 1][0] >= tmp[0] and input_list[i + 1][0] <= tmp[1] :
+            # then merge
+            start = min(tmp[0], input_list[i + 1][0])
+            # end time is bigger of the two
+            end = max(input_list[i][1], input_list[i + 1][1])
+
+            # keep tmp to merge other overlapping ranges
+            tmp = [start, end]
 
         else:
-            output.append([tup[0], tup[1]])
-            print "out", output
-            start = tup[0]
-            end = tup[1]
 
-    print "OUT: ", output
+            # no more merging, write out into the output list
+            output_list.append( ( tmp[0], tmp[1] ))
+            tmp = [input_list[i + 1][0], input_list[i + 1][1]]
+
+    output_list.append( (tmp[0], tmp[1] ))
+    return output_list
+
+
+# Even cleaner approach. Time efficiency O(n log n) and space O(n)
+def merge_ranges_clean(meetings):
+
+    # Sort
+    sorted_meetings = sorted(meetings)
+
+    # initialize merged_meetings with the earliest meeting
+    merged_meetings = [sorted_meetings[0]]
+
+    for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        # if the current and last meetings overlap, use the latest end time
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start, max(last_merged_meeting_end, current_meeting_end))
+
+
+        # add the current meeting since it doesn't overlap
+        else:
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
+
 
 
 
 
 # Test
+print(merge_ranges([(1, 5), (2, 3)]))
+print(merge_ranges_clean([(1, 5), (2, 3)]))
 # Should return [(0, 1), (3, 8), (9, 12)]
-merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+print(merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]))
+print(merge_ranges_clean([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]))
